72ce6453ffba8e0ac8e87fc30957eefb8d336c7a
Harry Sharma <harrysharma1066@gmail.com>
2024-11-01 20:57:59 +0000

    feat: add gitignore file and new http server

---
+++ server.js
@@ -0,0 +1,21 @@
+// import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+// const server = serve({port:6969});
+// console.log("Server running at https://localhost:6969/")
+
+// for await (const req of server){
+//     if (req.url=='/'){
+//         const file = Deno.readFile("index.html");
+//         req.respond({body:file});
+//     }else{
+//         req.respond({status:404, body:"Not Found"});
+//     }
+// }
+Deno.serve(async req => {
+    const file = await Deno.open("./index.html");
+    return new Response(file.readable, {
+        headers: {
+            "content-type" : "text/plain"
+        }
+    });
+});
